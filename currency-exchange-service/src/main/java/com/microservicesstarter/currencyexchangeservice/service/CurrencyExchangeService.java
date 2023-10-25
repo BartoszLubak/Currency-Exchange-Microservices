0b9504bb3ebbc4044e92b8ed1f049803ca6b648c
Bartosz Lubak <122176140+BartoszLubak@users.noreply.github.com>
2023-10-25 10:07:25 +0200

    Update CurrencyExchangeService.java
    add currency exchange api

--- currency-exchange-service/src/main/java/com/microservicesstarter/currencyexchangeservice/service/CurrencyExchangeService.java
+++ currency-exchange-service/src/main/java/com/microservicesstarter/currencyexchangeservice/service/CurrencyExchangeService.java
@@ -1,22 +1,34 @@
 package com.microservicesstarter.currencyexchangeservice.service;
 
+import com.microservicesstarter.currencyexchangeservice.dto.CurrencyDto;
 import com.microservicesstarter.currencyexchangeservice.model.CurrencyExchange;
-import com.microservicesstarter.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
 
 @Service
 public class CurrencyExchangeService {
-    private final CurrencyExchangeRepository currencyExchangeRepository;
+    private final CurrencyWebClient currencyWebClient;
 
-    public CurrencyExchangeService(CurrencyExchangeRepository currencyExchangeRepository) {
-        this.currencyExchangeRepository = currencyExchangeRepository;
+    public CurrencyExchangeService(CurrencyWebClient currencyWebClient) {
+        this.currencyWebClient = currencyWebClient;
     }
+
 
     public CurrencyExchange findCurrencyExchangeByFromAndTo(String from, String to) {
-        CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
-        if (currencyExchange==null){
-            throw new RuntimeException("Unable to find data for " + from + " to " + to);
-        }
-        return  currencyExchange;
+        CurrencyExchange currencyExchange = new CurrencyExchange();
+        currencyExchange.setFrom(from);
+        currencyExchange.setTo(to);
+        currencyExchange.setConversionMultiple(currencyExchangeToUSD(to).divide(currencyExchangeToUSD(from), 2, RoundingMode.HALF_UP));
+        return currencyExchange;
     }
+
+    public BigDecimal currencyExchangeToUSD(String currencyCode) {
+        CurrencyDto currencyDto = currencyWebClient.getCurrencyData();
+        Map<String, BigDecimal> rates = currencyDto.getRates();
+        return rates.get(currencyCode);
+    }
+
 }
